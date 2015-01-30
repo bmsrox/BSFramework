b55d7668a15c1e54e36071944e6ef99f0346cad0
Bruno Santos <bmsrox@gmail.com>
2015-01-30 15:43:17 -0200

    correção no render da sitecontroller

--- module/Application/controllers/SiteController.php
+++ module/Application/controllers/SiteController.php
@@ -6,11 +6,11 @@ use Application\components\Controller;
 class SiteController extends Controller{
 
 	public function actionIndex() {
-		return $this->render('index', array('model'=>'Page Index');
+		return $this->render('index', array('model'=>'Page Index'));
 	}
 
 	public function actionAbout() {
-		return $this->render('about', array('model'=>'Page About');
+		return $this->render('about', array('model'=>'Page About'));
 	}
 
 	public function actionError() {
